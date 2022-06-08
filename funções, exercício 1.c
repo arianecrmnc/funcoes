2adc1fe2e00ce378aea4a323321d3b573074f2f6
arianecrmnc <101533612+arianecrmnc@users.noreply.github.com>
2022-06-08 18:36:10 -0300

    Update funções, exercício 1.c
    fiz errado

--- funções, exercício 1.c
+++ funções, exercício 1.c
@@ -3,22 +3,4 @@
 int _eh_bissexto (int ano);
 int main ()
 {
-    int _eh_bissexto (int ano)
-    {
-        if (ano%4==0 && (ano%100!=0 || ano%400==0))
-            return 1;
-        else
-            return 0;
-    }
-
-    int ano_escolhido;
-
-    printf("informe um ano:");
-    scanf("%d", &ano_escolhido);
-    if (_eh_bissexto(ano_escolhido))
-        printf("eh bissexto");
-    else
-        printf("nao eh bissexto");
-
-    return 0;
-}
+    int _eh_bissexto (ikjnkjkj
